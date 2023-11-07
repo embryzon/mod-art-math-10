334666a5ac7229d79c8bdb3a5a28fa7dc340f351
embryzon <95128949+embryzon@users.noreply.github.com>
2023-11-07 21:19:18 +0800

    .

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,15 +1,15 @@
-// import truncatedTable from "../modules/TruncatedTable.mjs";
+import truncatedTable from "../modules/TruncatedTable.mjs";
 
-let arrayPattern = [
-    [6, 5, 4, 3, 2, 1], 
-    [4, 1, 5, 2, 6, 3],
-    [5, 3, 1, 6, 4, 2],
-    [2, 4, 6, 1, 3, 5],
-    [3, 6, 2, 5, 1, 4],
-    [1, 2, 3, 4, 5, 6]
-]
+// let arrayPattern = [
+//     [6, 5, 4, 3, 2, 1], 
+//     [4, 1, 5, 2, 6, 3],
+//     [5, 3, 1, 6, 4, 2],
+//     [2, 4, 6, 1, 3, 5],
+//     [3, 6, 2, 5, 1, 4],
+//     [1, 2, 3, 4, 5, 6]
+// ]
 
-let truncatedTable = arrayPattern
+// let truncatedTable = arrayPattern
 const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 
